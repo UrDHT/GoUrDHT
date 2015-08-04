62de8b31a88d99dadbf3cf1e89de8716cb9c43a6
abrosen <rosen@cs.gsu.edu>
2015-08-04 13:43:17 -0400

    Okay, networking ping "works"

--- Network.go
+++ Network.go
@@ -7,17 +7,24 @@ see: https://github.com/UrDHT/DevelopmentPlan/blob/master/Network.md
 
 */
 
-package GoUrDHT
+package main
 
-import "encoding/json"
+//import "encoding/json"
+import "net/http"
+import "fmt"
+import "io/ioutil"
+
+var version string = "api/v0/"
+var clientURL string = version+"client/"
+var peerURL string = version + "peer/"
 
 type Networking struct {
 	ip, port string
 }
-
+/*
 func (nw Networking) setup(logic DHTLogic, database DataBase) {
 	//TODO: what is the server type?
-	server.setLinks(logic, database)
+	//server.setLinks(logic, database)
 }
 
 func (nw Networking) seek(remote, id string) string {
@@ -32,8 +39,20 @@ func (nw Networking) notify(remote, origin string) bool {
 func (nw Networking) store(remote, id, data string) bool {
 }
 
-func (nw Networking) getIP(remote string) string {
+func (nw Networking) GetIP(remote string) string {
+}
+*/
+//func (nw Networking) Ping(remote string) string {
+func Ping(remote string) {
+	resp, _  := http.Get(remote+version+peerURL+"ping")
+	text, _ :=  ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	
+	fmt.Printf("The repsonse is %s", text)
 }
 
-func (nw Networking) ping(remote string) string {
+
+func main() {
+	target := "http://envy.blamestross.com:8000/"
+	Ping(target)
 }
